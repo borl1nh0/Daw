cf0ca2300a016d5e6ec72237b983dd73fa4deb54
Juan Antonio Sancho <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-09-26 08:31:05 +0200

    sdf

---
+++ DWEC/mensaje.js
@@ -0,0 +1,30 @@
+//let numero = 5;
+//let tipo = (numero % 2 == 0) ? "par" : "impar";
+//console.log(tipo);
+
+//let numero = parseInt (prompt("Introduce un numero: "))
+
+
+
+    //for (let i = 0; i < numero; i++) {
+    
+    //console.log("El numero es " + ((numero % 2 == 0) ? "par" : "impar"))
+    //}
+
+//si el numero es multiplo de 3 que diga viva el sevilla si es multiplo de 5 que viva el betis sy es multiplo de 15 viva el sevilla y el betis
+
+let numero = 40
+  for (let i = 0; i < numero; i++) {
+    
+    if (numero % 3 == 0) {
+        console.log("Viva el Sevilla")
+        console.
+      
+    if (numero % 5 == 0) {
+        console.log("Viva el Betis")
+    }
+    if (numero % 15 == 0) {
+        console.log("Viva el Sevilla y el Betis")
+     }
+    } 
+}
