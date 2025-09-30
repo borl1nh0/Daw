1307fa000982dc44259456f6cd04ac9a55229785
Juan Antonio Sancho Hidalgo <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-09-30 17:52:09 +0200

    nueva rama

--- Django/protectora_animales/mysite/settings.py
+++ Django/protectora_animales/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'protectora',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'mysite.urls'
