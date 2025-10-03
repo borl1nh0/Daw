fc54361a5debb2315eac08a89ca2179ed3b20269
Juan Antonio Sancho Hidalgo <124882764+borl1nh0@users.noreply.github.com>
2025-10-03 10:38:24 +0200

    Delete Django/protectora_animales directory

--- Django/protectora_animales/protectora/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Animal, Protectora, Colaborador
-# Register your models here.
-admin.site.register(Animal)
-admin.site.register(Protectora)
-admin.site.register(Colaborador)
-
