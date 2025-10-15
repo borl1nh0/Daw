2f70b22e62af318a298c5679f98ab9349c85523b
Juan Antonio Sancho <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-10-15 12:53:03 +0200

    Fix variable declaration for arrayCuadrado in arrayCuadrado.js

--- DWEC/JavaScript/arrayCuadrado.js
+++ DWEC/JavaScript/arrayCuadrado.js
@@ -1,5 +1,5 @@
 // Crear el array original
 const arryOriginal = [1, 2, 3, 4, 5, 6, 7];
 
-const arrayCuadrado = arryOriginal.forEach((valor)=> console.log(valor + " su cuadrado es : " + (valor ** 2)));
+arrayCuadrado = arryOriginal.forEach((valor)=> console.log(valor + " su cuadrado es : " + (valor ** 2)));
  
